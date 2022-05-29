cac5ad9c3466492fab45e5651249aaf383a2dcd3
saba <105594163+SabaH4@users.noreply.github.com>
2022-05-29 21:34:29 +0500

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,26 @@
+public class Literals 
+{
+  public static void main (String args[])
+  {
+    int a=50;
+	System.out.println(a);
+	int b=062;
+	System.out.println(b);
+	int c=0x32;
+	System.out.println(c);
+	float d=3.1415926f;
+	System.out.println(d);
+	double e=3.1415926;
+	System.out.println(e);
+	double f=3.1415e5;
+	System.out.println(f);
+	double g=0x3.Ep1;
+	System.out.println(g);
+	char h=65;
+	System.out.println(h);
+	char i='\101';
+	System.out.println(i);
+	char j='\u0041';
+	System.out.println(j);
+  }
+}
